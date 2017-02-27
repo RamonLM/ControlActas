cd4ef0a826d1cc10df3fd4c1dfb3eb764f3ed3ab
RamonLM <ramon.lm@icloud.com>
2017-02-26 22:30:55 -0800

    Website layout and MVC Implementation

---
+++ src/ControlActas/Services/IMailService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlActas.Services
+{
+    public interface IMailService
+    {
+        void sendMail(string to, string from, string subject, string body);
+    }
+}
---
+++ src/ControlActas/Services/MailServiceImpl.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlActas.Services
+{
+    public class MailServiceImpl : IMailService
+    {
+        public void sendMail(string to, string from, string subject, string body)
+        {
+            Debug.WriteLine($"Sending Mail To: {to} From: {from} Subject: {subject}");
+        }
+    }
+}
